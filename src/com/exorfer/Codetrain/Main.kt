5719d02eff42f6806f86b3264c138a6ba1dc4f0e
exorfer <exorfer@gmail.com>
2019-03-23 05:48:32 +0000

    it it it

--- src/com/exorfer/Codetrain/Main.kt
+++ src/com/exorfer/Codetrain/Main.kt
@@ -5,18 +5,18 @@ package com.exorfer.Codetrain
 fun main(args: Array<String>) {
     val status = when (args.size){
         0 -> getStatus(0) {
-            println("No args!!!")
+            println("No args!!! Status @it")
         }
         1 -> getStatus(1)
         2 -> getStatus(2)
         else -> getStatus(100) {
-            args.forEach { println(it) }
+            args.forEach { i -> println(i) }
         }
     }
     System.exit(status);
 }
 
-fun getStatus(status: Int, callback: () -> Unit = {}): Int {
-    callback()
+fun getStatus(status: Int, callback: (st: Int) -> Unit = {}): Int {
+    callback(status)
     return status
 }
