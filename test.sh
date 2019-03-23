191b30b382c16765e050b4c89825bc187cb618cb
exorfer <exorfer@gmail.com>
2019-03-23 04:04:38 +0000

    count of test

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 ./BUILD.SH
-
+COUNT_ALL=0
+COUNT_SUC=0
 echo
 echo No param test
 ./run.sh
 RES=$?
-if [ $? -eq 0 ]; then
+let COUNT_ALL++
+if [ $RES -eq 0 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 0 Actual ${RES}
 fi
@@ -16,18 +19,21 @@ echo
 echo 1 args
 ./run.sh foo
 RES=$?
-if [ $? -eq 1 ]; then
+let COUNT_ALL++
+if [ $RES -eq 1 ]; then
     echo test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 1 Actual ${RES}
 fi
-
 echo
 echo 2 args
 ./run.sh foo bar
 RES=$?
-if [ $? -eq 2 ]; then
+let COUNT_ALL++
+if [ $RES -eq 2 ]; then
     echo test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 2 Actual ${RES}
 fi
@@ -36,8 +42,19 @@ echo
 echo Many args
 ./run.sh foo bar baz "1 2 3"
 RES=$?
-if [ $? -eq 100 ]; then
+let COUNT_ALL++
+if [ $RES -eq 100 ]; then
     echo test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 100 Actual ${RES}
+fi
+
+echo ***
+if [ $COUNT_ALL -eq $COUNT_SUC ]; then
+    echo All $COUNT_ALL tests passed
+    exit 0
+else
+    echo $COUNT_SUC of $COUNT_ALL test passed
+exit 1
 fi
