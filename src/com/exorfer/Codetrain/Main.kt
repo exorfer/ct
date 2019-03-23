5394ea1a9613b70d7dec8d7bd131491956640708
exorfer <exorfer@gmail.com>
2019-03-23 05:14:22 +0000

    lift when

--- src/com/exorfer/Codetrain/Main.kt
+++ src/com/exorfer/Codetrain/Main.kt
@@ -1,22 +1,17 @@
 package com.exorfer.Codetrain
 
 fun main(args: Array<String>) {
-    when {
+    val status = when {
         args.isEmpty() -> {
             println("No Args!!!111")
-            System.exit(0)
+            0
         }
-        args.size == 1 -> {
-            args.forEach { println(it) }
-            System.exit(1)
-        }
-        args.size == 2 -> {
-            args.forEach { println(it) }
-            System.exit(2)
-        }
+        args.size == 1 -> 1
+        args.size == 2 -> 2
         else -> {
             args.forEach { println(it) }
-            System.exit(100)
+            100
         }
     }
+    System.exit(status);
 }
