3f6d9be42c2414570d96eda0f710c3ffdfb31d97
exorfer <exorfer@gmail.com>
2019-03-23 04:51:40 +0000

    Add codes

--- src/com/exorfer/Codetrain/Main.kt
+++ src/com/exorfer/Codetrain/Main.kt
@@ -3,7 +3,19 @@ package com.exorfer.Codetrain
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("No Args!!!111")
-    } else {
-        args.forEach { println(it) }
-    }
+        System.exit(0)
+    } else
+        if (args.size == 1) {
+            args.forEach { println(it) }
+            System.exit(1)
+        }
+            else
+            if (args.size == 2) {
+                args.forEach { println(it) }
+                System.exit(2)
+            }
+            else {
+                args.forEach { println(it) }
+                System.exit(100)
+            }
 }
