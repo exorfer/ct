63546d654c66ca2c7ac79c8c93240be0b75882f4
exorfer <exorfer@gmail.com>
2019-03-23 04:56:47 +0000

    if -> when

--- src/com/exorfer/Codetrain/Main.kt
+++ src/com/exorfer/Codetrain/Main.kt
@@ -1,21 +1,22 @@
 package com.exorfer.Codetrain
 
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        println("No Args!!!111")
-        System.exit(0)
-    } else
-        if (args.size == 1) {
+    when {
+        args.isEmpty() -> {
+            println("No Args!!!111")
+            System.exit(0)
+        }
+        args.size == 1 -> {
             args.forEach { println(it) }
             System.exit(1)
         }
-            else
-            if (args.size == 2) {
-                args.forEach { println(it) }
-                System.exit(2)
-            }
-            else {
-                args.forEach { println(it) }
-                System.exit(100)
-            }
+        args.size == 2 -> {
+            args.forEach { println(it) }
+            System.exit(2)
+        }
+        else -> {
+            args.forEach { println(it) }
+            System.exit(100)
+        }
+    }
 }
